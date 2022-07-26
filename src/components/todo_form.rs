327a2eb7ce585ed98a86f9e896f3936613a52e09
Nick Landreville <n1ck@hey.com>
2022-07-25 19:18:42 -0500

    post functionality

--- src/components/todo_form.rs
+++ src/components/todo_form.rs
@@ -4,37 +4,42 @@ use crate::components::text_input::TextInput;
 
 use crate::components::custom_button::CustomButton;
 
-// #[derive(Properties, PartialEq)]
-// pub struct Props {
-//     onsubmit: Callback<()>,
-// }
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub onsubmit: Callback<Data>,
+}
 
 #[derive(Default, Clone)]
-struct Data {
+pub struct Data {
    pub todo: String,
 }
 
 
 #[function_component(TodoForm)]
-pub fn todo_form() -> Html {
+pub fn todo_form(props: &Props) -> Html {
 
     let state = use_state(|| Data::default());
     let cloned_state = state.clone();
     let todo_changed = Callback::from(move |todo| {
-        // let mut data = cloned_state.deref().clone();
-        // data.todo = todo;
-        // cloned_state.set(data)
         cloned_state.set(
             Data{todo, ..cloned_state.deref().clone()}
         )
     });
 
+    let onsubmit_clone = props.onsubmit.clone();
+    let cloned_state = state.clone();
+    let onsubmit = Callback::from(move |e: FocusEvent| {
+        e.prevent_default();
+
+        let data = cloned_state.deref().clone();
+        onsubmit_clone.emit(data);
+    });
 
     html! {
-        <div>
+        <form onsubmit={onsubmit}>
         <p>{"Todo: "}{&state.todo}</p>
         <TextInput name="Todo" handle_onchange={todo_changed}/>
         <CustomButton label="Add Todo!!!!!!"  />
-        </div>
+        </form>
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,17 @@ use yew::prelude::*;
 mod components;
 use components::main_title::{MainTitle, Color};
 use components::todo_form::TodoForm;
+use crate::components::todo_form::Data;
 
 #[styled_component(App)]
 pub fn app() -> Html {
+    let form_submit = Callback::from(|data: Data| {
+
+    });
     html! {
         <div>
         <MainTitle title="Todo List" color={Color::Dark} />
-        <TodoForm />
+        <TodoForm onsubmit={form_submit}/>
         </div >
     }
 }
